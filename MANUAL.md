51efc200a076c092bfb9016bb758bcbab63e174b
Guillaume Anthouard <guillaume.anthouard@hotmail.fr>
2021-05-25 15:16:08 +0200

    Rework shader loader, allow loading WGSL shaders

--- MANUAL.md
+++ MANUAL.md
@@ -1,12 +1,20 @@
 # Nuance shaders
 
-Shaders are written with the Vulkan flavor of GLSL (`#version 460`).
+Shaders are preferably written with the Vulkan flavor of GLSL (`#version 460`).
 The [GL_KHR_vulkan_glsl](https://github.com/KhronosGroup/GLSL/blob/master/extensions/khr/GL_KHR_vulkan_glsl.txt)
 extension is implicitly enabled.
 
 Some great resources to learn about shaders :
 
 - [thebookofshaders.com](https://thebookofshaders.com/)
+
+## Supported languages
+
+Support|GLSL|WGSL|Rust|SpirV
+-------|----|----|----|-----
+Import |✔️  |✔️  |    |✔️
+Std    |✔️  |    |    |
+Params |✔️  |    |    |
 
 ## Shader output
 
--- src/shader_loader.rs
+++ src/shader_loader.rs
@@ -1,9 +1,10 @@
 use std::borrow::Cow;
+use std::ffi::OsStr;
 use std::fs;
 use std::path::Path;
 
 use anyhow::{anyhow, Result};
-use log::{debug, error};
+use log::{debug, error, warn};
 use shaderc::{
     CompileOptions, Compiler, EnvVersion, GlslProfile, IncludeType, OptimizationLevel,
     ResolvedInclude, ShaderKind, SourceLanguage, TargetEnv,
@@ -39,48 +40,55 @@ impl ShaderLoader {
     /// Load a shader, this will try to guess its type based on the file extension
     pub fn load_shader<P: AsRef<Path>>(&mut self, path: P) -> Result<(Shader, ShaderSource)> {
         let path = path.as_ref();
-        // Already compiled shader
-        if path.extension().map_or(false, |e| e == "spv") {
-            // sry for that terrible thing
-            let data: Vec<u32> = fs::read(path)?.into_iter().map(|i| i as u32).collect();
-            //debug!("data : {:#x?}", data);
-            // We can't extract metadata from spirv modules
-            Ok((
+        // TODO collect all files necessary to compilation for watch
+        match path.extension().and_then(|it| it.to_str()) {
+            Some("spv") => {
+                // sry for that terrible thing
+                let data: Vec<u32> = fs::read(path)?.into_iter().map(|i| i as u32).collect();
+                // We can't extract metadata from spirv modules
+                Ok((
+                    Shader {
+                        main: path.to_path_buf(),
+                        sources: vec![path.to_path_buf()],
+                        metadata: None,
+                    },
+                    ShaderSource::SpirV(Cow::Owned(data)),
+                ))
+            }
+            Some("glsl") | Some("frag") => {
+                // Preprocess glsl to extract what we need
+                let mut source = fs::read_to_string(path)?;
+                let metadata = if let Ok((metadata, new)) = preprocessor::extract(&source) {
+                    // We found params and transpiled the code
+                    source = new;
+                    Some(metadata)
+                } else {
+                    // No params extracted and source isn't modified
+                    None
+                };
+
+                self.compile_shader(path.to_str().unwrap(), &source, "main")
+                    .map(|it| {
+                        (
+                            Shader {
+                                main: path.to_path_buf(),
+                                sources: vec![path.to_path_buf()],
+                                metadata,
+                            },
+                            it,
+                        )
+                    })
+            }
+            Some("wgsl") => Ok((
+                // TODO extract data from wgsl
                 Shader {
                     main: path.to_path_buf(),
                     sources: vec![path.to_path_buf()],
                     metadata: None,
                 },
-                ShaderSource::SpirV(Cow::Owned(data)),
-            ))
-        } else if path
-            .extension()
-            .map_or(false, |e| e == "frag" || e == "glsl")
-        {
-            // Preprocess glsl to extract what we need
-            let mut source = fs::read_to_string(path)?;
-            let metadata = if let Ok((metadata, new)) = preprocessor::extract(&source) {
-                // We found params and transpiled the code
-                source = new;
-                Some(metadata)
-            } else {
-                // No params extracted and source isn't modified
-                None
-            };
-
-            self.compile_shader(path.to_str().unwrap(), &source, "main")
-                .map(|it| {
-                    (
-                        Shader {
-                            main: path.to_path_buf(),
-                            sources: vec![path.to_path_buf()],
-                            metadata,
-                        },
-                        it,
-                    )
-                })
-        } else {
-            Err(anyhow!("Unsupported shader format !"))
+                ShaderSource::Wgsl(Cow::Owned(fs::read_to_string(path)?)),
+            )),
+            _ => Err(anyhow!("Unsupported shader format !")),
         }
     }
 
@@ -91,50 +99,36 @@ impl ShaderLoader {
         source: &str,
         entrypoint: &str,
     ) -> Result<ShaderSource<'_>> {
-        let includes = &self.include_dirs;
-        let mut options = CompileOptions::new().unwrap();
-        options.set_source_language(SourceLanguage::GLSL);
-        // Required so we can introspect the shaders
-        options.set_generate_debug_info();
-        options.set_optimization_level(OptimizationLevel::Performance);
-        options.set_target_env(TargetEnv::Vulkan, EnvVersion::WebGPU as u32);
+        let mut opts = CompileOptions::new().unwrap();
+        opts.set_source_language(SourceLanguage::GLSL);
+        opts.set_optimization_level(OptimizationLevel::Performance);
+        opts.set_target_env(TargetEnv::Vulkan, EnvVersion::WebGPU as u32);
         //options.set_target_spirv(SpirvVersion::V1_5);
-        options.set_forced_version_profile(460, GlslProfile::None);
-        options.set_include_callback(|name, include_type, source_file, _| {
-            Self::find_include(includes, name, include_type, source_file)
+        opts.set_forced_version_profile(460, GlslProfile::None);
+
+        let include_dirs = &self.include_dirs;
+        opts.set_include_callback(move |name, include_type, source_file, _| {
+            Self::find_include(include_dirs, name, include_type, source_file)
         });
+
         let compiled = self.compiler.compile_into_spirv(
             source,
             ShaderKind::Fragment,
             name,
             entrypoint,
-            Some(&options),
+            Some(&opts),
         )?;
+
+        if compiled.get_num_warnings() > 0 {
+            warn!(
+                "Compilation warnings : \n{}",
+                compiled.get_warning_messages()
+            );
+        }
+
         Ok(ShaderSource::SpirV(Cow::Owned(
             compiled.as_binary().to_owned(),
         )))
-    }
-
-    pub fn preprocess(&mut self, name: &str, source: &str, entrypoint: &str) {
-        let includes = &self.include_dirs;
-        let mut options = CompileOptions::new().unwrap();
-        options.set_source_language(SourceLanguage::GLSL);
-        // Required so we can introspect the shaders
-        options.set_generate_debug_info();
-        options.set_optimization_level(OptimizationLevel::Performance);
-        options.set_target_env(TargetEnv::Vulkan, EnvVersion::WebGPU as u32);
-        //options.set_target_spirv(SpirvVersion::V1_5);
-        options.set_forced_version_profile(460, GlslProfile::None);
-        options.set_include_callback(|name, include_type, source_file, _| {
-            Self::find_include(includes, name, include_type, source_file)
-        });
-        let result = self
-            .compiler
-            .preprocess(source, name, entrypoint, Some(&options));
-        match result {
-            Ok(inner) => debug!("preprocessed : {}", inner.as_text()),
-            Err(e) => error!("Nooooo ! {}", e),
-        }
     }
 
     /// Resolve an include with the given name
@@ -147,12 +141,14 @@ impl ShaderLoader {
         match include_type {
             IncludeType::Relative => {
                 let local_inc = Path::new(source_file).parent().unwrap().join(name);
+                // Search in the shader directory
                 if local_inc.exists() {
                     Ok(ResolvedInclude {
                         resolved_name: local_inc.to_str().unwrap().to_string(),
                         content: fs::read_to_string(&local_inc).map_err(|e| e.to_string())?,
                     })
                 } else {
+                    // Search in registered include dirs
                     includes
                         .iter()
                         .find_map(|dir| {
@@ -170,6 +166,7 @@ impl ShaderLoader {
                 }
             }
             IncludeType::Standard => {
+                // The nuance standard header
                 if name == "Nuance" {
                     const STD: &str = include_str!("Nuance.glsl");
                     Ok(ResolvedInclude {
